4c2e5dd31ce73d0476a34465fa02ae4219703e87
Timo Pfaff <TimoPfaff88@gmx.de>
2018-07-25 00:06:18 +0200

    Refactoring-AWSDfd8OMDLKieESzADK

--- Calculator/src/main/java/calculator/Calculator.java
+++ Calculator/src/main/java/calculator/Calculator.java
@@ -1,24 +1,26 @@
 package calculator;
 
-public class Calculator extends SuperCalculator{
+public class Calculator extends SuperCalculator {
 
-	private String resultAsText;
-	
-	public final static int MULTIPLIER = 10;
+    private String resultAsText;
 
-	public double addition(double a, double b) {
-		return a + b;
-	}
+    public static final int MULTIPLIER = 10;
 
-	public double multiplikation(double a, double b, double c) {
-		return a * b;
-	}
+    @Override
+    public double addition(double a, double b) {
+        return a + b;
+    }
 
-	public double subtraction(double a, double b) {
-		return a - b;
-	}
+    public double multiplikation(double a, double b, double c) {
+        return a * b;
+    }
 
-	public double division(double a, double b) {
-		return a / b;
-	}
+    public double subtraction(double a, double b) {
+        return a - b;
+    }
+
+    public double division(double a, double b) {
+        return a / b;
+    }
 }
+
