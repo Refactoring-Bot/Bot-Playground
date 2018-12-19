c5df7a35bea05ec5b787a36ac071d0cad2841475
dennisRef145 <wert45wert@web.de>
2018-12-19 15:14:20 +0100

    Reordered modifier

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,12 +6,12 @@ public class Calculator extends SuperCalculator {
 
     private String resultAsText;
 
-    public final static int MULTIPLIER = 10;
+    public static final int MULTIPLIER = 10;
 
     public static void main(String[] args) {
         new Calculator().runProgram();
     }
-    
+
     public double addition(double a, double b) {
         return a + b;
     }
@@ -20,7 +20,9 @@ public class Calculator extends SuperCalculator {
         return a * b;
     }
 
-    public double multiplication(double a, double b) { return multiplication(a, b, 42); }
+    public double multiplication(double a, double b) {
+        return multiplication(a, b, 42);
+    }
 
     public double subtraction(double a, double b) {
         return a - b;
@@ -32,52 +34,46 @@ public class Calculator extends SuperCalculator {
 
     public void runProgram() {
         println("The calculator is ready to calculate!");
-        
         // get numbers
-        Scanner inp= new Scanner(System.in);
+        Scanner inp = new Scanner(System.in);
         int num1;
         int num2;
         println("Enter first number:");
         num1 = inp.nextInt();
         println("Enter second number:");
         num2 = inp.nextInt();
-
         // get operation
         println("Enter your selection: 1 for addition, 2 for subtraction, 3 for multiplication and 4 for division:");
         int choose;
         choose = inp.nextInt();
-
         // calculate and print result
-        switch (choose){
+        switch(choose) {
             case 1:
                 println("The result of the addition is:");
-                println(addition( num1,num2));
+                println(addition(num1, num2));
                 break;
             case 2:
                 println("The result of the subtraction is:");
-                println(subtraction( num1,num2));
+                println(subtraction(num1, num2));
                 break;
             case 3:
                 println("The result of the multiplication is:");
-                println(multiplication( num1,num2));
+                println(multiplication(num1, num2));
                 break;
             case 4:
                 println("The result of the division is:");
-                println(division( num1,num2));
+                println(division(num1, num2));
                 break;
             default:
                 println("Illegal Operation");
         }
-
         inp.close();
         println("Thank you for using this awesome calculator.");
     }
 
     private void println(Object object) {
-        System.out.println(object); //NOSONAR
+        // NOSONAR
+        System.out.println(object);
     }
-    
 }
-
-
 
